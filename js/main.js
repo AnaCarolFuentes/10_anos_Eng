dd0704728d22666599f73203d6295e055abf0d31
Rafael Rarvs <rafael.rarvs@gmail.com>
2024-09-26 16:49:31 -0300

    inicial

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.carousel').carousel({
+        interval: 2000,
+        wrap: true,
+    });
+
+    $(".nav .nav-link").on("click", function () {
+        $(".nav").find(".active").removeClass("active");
+        $(this).addClass("active");
+    });
+})
